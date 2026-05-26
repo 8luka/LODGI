daaa9a84f8c60c6bdd7b1f6b54c89dfc533c1a2a
João Santos <joao.saul.santos@gmail.com>
2026-05-26 10:57:25 +0100

    map v2 - with pins on the locations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @properties = JSON.generate(Property.select(:latitude, :longitude).as_json(except: [:id]))
   end
 end
--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="maps"
 export default class extends Controller {
+  static values = {
+    properties: Array
+  }
+
   connect() {
-  const center =  { lat: 35.6764, lng: 139.6500 };
-  const map = new google.maps.Map(this.element, {
-    center,
-    zoom: 12,
-    mapId: "DEMO_MAP_ID",
-  });
+    console.log(google)
+    const center = { lat: 35.6764, lng: 139.6500 };
+    const map = new google.maps.Map(this.element, {
+      center,
+      zoom: 12,
+      mapId: "DEMO_MAP_ID",
+    });
+    this.propertiesValue.forEach((property) => {
+      const marker = new google.maps.marker.AdvancedMarkerElement({
+        map,
+        position: { lat: property.latitude, lng: property.longitude }
+      })
+    })
   }
 }
