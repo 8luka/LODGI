d528a0f0faf7e2e101885c1f98ddbfbd1b6aacdd
Deniz Caner <dencaner94@gmail.com>
2026-06-01 17:40:37 +0900

    favorite html,scss,changes in controllers and route,showpageneighb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_31_153917) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_01_044336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,24 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_31_153917) do
     t.string "icon"
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.boolean "blocked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.bigint "favoritable_id", null: false
+    t.string "favoritable_type", null: false
+    t.bigint "favoritor_id", null: false
+    t.string "favoritor_type", null: false
+    t.string "scope", default: "favorite", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blocked"], name: "index_favorites_on_blocked"
+    t.index ["favoritable_id", "favoritable_type"], name: "fk_favoritables"
+    t.index ["favoritable_type", "favoritable_id", "favoritor_type", "favoritor_id", "scope"], name: "uniq_favorites__and_favoritables", unique: true
+    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable"
+    t.index ["favoritor_id", "favoritor_type"], name: "fk_favorites"
+    t.index ["favoritor_type", "favoritor_id"], name: "index_favorites_on_favoritor"
+    t.index ["scope"], name: "index_favorites_on_scope"
   end
 
   create_table "inquiries", force: :cascade do |t|
